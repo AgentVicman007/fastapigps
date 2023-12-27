7464108bd7d776b786d92024a18ae80a7bc5d608
AgentVicman007 <69023921+AgentVicman007@users.noreply.github.com>
2023-12-26 16:57:40 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
+# from fastapi import FastAPI, HTTPException
+
+# app = FastAPI()
+
+# # Single endpoint to capture payload from either URL parameters or JSON data
+# @app.post("/")
+# async def capture_payload(id: int = None, timestamp: int = None, lat: float = None, lon: float = None,
+#                           speed: float = None, bearing: float = None, altitude: float = None,
+#                           accuracy: float = None, batt: int = None, raw_payload: str = None):
+#     if raw_payload is not None:
+#         # Payload from raw data
+#         print("Raw Payload received:", raw_payload)
+#         return {"message": "Raw Payload captured successfully", "payload": raw_payload}
+#     elif any(arg is None for arg in [id, timestamp, lat, lon, speed, bearing, altitude, accuracy, batt]):
+#         # Check for missing parameters
+#         raise HTTPException(status_code=400, detail="Invalid payload format")
+#     else:
+#         # Payload from URL parameters
+#         payload = {"id": id, "timestamp": timestamp, "lat": lat, "lon": lon,
+#                    "speed": speed, "bearing": bearing, "altitude": altitude,
+#                    "accuracy": accuracy, "batt": batt}
+#         print("Payload received:", payload)
+#         return {"message": "Payload captured successfully", "payload": payload}
 from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 # Single endpoint to capture payload from either URL parameters or JSON data
 @app.post("/")
-async def capture_payload(id: int = None, timestamp: int = None, lat: float = None, lon: float = None,
-                          speed: float = None, bearing: float = None, altitude: float = None,
-                          accuracy: float = None, batt: int = None, raw_payload: str = None):
-    if raw_payload is not None:
+async def capture_payload(**kwargs):
+    if kwargs:
         # Payload from raw data
-        print("Raw Payload received:", raw_payload)
-        return {"message": "Raw Payload captured successfully", "payload": raw_payload}
-    elif any(arg is None for arg in [id, timestamp, lat, lon, speed, bearing, altitude, accuracy, batt]):
-        # Check for missing parameters
-        raise HTTPException(status_code=400, detail="Invalid payload format")
+        print("Payload received:", kwargs)
+        return {"message": "Payload captured successfully", "payload": kwargs}
     else:
-        # Payload from URL parameters
-        payload = {"id": id, "timestamp": timestamp, "lat": lat, "lon": lon,
-                   "speed": speed, "bearing": bearing, "altitude": altitude,
-                   "accuracy": accuracy, "batt": batt}
-        print("Payload received:", payload)
-        return {"message": "Payload captured successfully", "payload": payload}
+        raise HTTPException(status_code=400, detail="Invalid payload format")

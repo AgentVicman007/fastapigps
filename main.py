dfdd904100fb1eb3ba3b17603b067ff66e10564e
AgentVicman007 <69023921+AgentVicman007@users.noreply.github.com>
2023-12-26 16:36:15 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ app = FastAPI()
 
 # Single endpoint to capture payload from either URL parameters or JSON data
 @app.post("/capture_payload")
-async def capture_payload(data: dict = None, param1: str = None, param2: int = None):
-    if data is not None:
-        # Payload from JSON data
-        payload = data
-    elif param1 is not None and param2 is not None:
-        # Payload from URL parameters
-        payload = {"param1": param1, "param2": param2}
-    else:
+async def capture_payload(id: int = None, timestamp: int = None, lat: float = None, lon: float = None,
+                          speed: float = None, bearing: float = None, altitude: float = None,
+                          accuracy: float = None, batt: int = None, raw_payload: str = None):
+    if raw_payload is not None:
+        # Payload from raw data
+        print("Raw Payload received:", raw_payload)
+        return {"message": "Raw Payload captured successfully", "payload": raw_payload}
+    elif any(arg is None for arg in [id, timestamp, lat, lon, speed, bearing, altitude, accuracy, batt]):
+        # Check for missing parameters
         raise HTTPException(status_code=400, detail="Invalid payload format")
-
-    print("Payload received:", payload)
-    return {"message": "Payload captured successfully", "payload": payload}
+    else:
+        # Payload from URL parameters
+        payload = {"id": id, "timestamp": timestamp, "lat": lat, "lon": lon,
+                   "speed": speed, "bearing": bearing, "altitude": altitude,
+                   "accuracy": accuracy, "batt": batt}
+        print("Payload received:", payload)
+        return {"message": "Payload captured successfully", "payload": payload}

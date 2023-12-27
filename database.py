3ec0ff72ff60b4f17d91c2d1e686ca41508b015a
AgentVicman007 <69023921+AgentVicman007@users.noreply.github.com>
2023-12-26 19:55:23 -0800

    Update database.py

--- database.py
+++ database.py
@@ -1,56 +1,6 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-
-
-
-# from urllib.parse import quote_plus
-
-# # Original password
-# raw_password = "Cr!st0V!v3J3susS@lvA2023!"
-
-# # URL-encode the password
-# encoded_password = quote_plus(raw_password)
-
-# # Construct the database URL with the encoded password
-# SQLALCHEMY_DATABASE_URL = f"postgresql://gpstracker2:{encoded_password}@localhost/raw_data_db"
-
-
-
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-# # Prod 
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from urllib.parse import quote_plus
-
-# # New database credentials
-# username = "postgres"
-# password = "Josiah1!"
-# host = "localhost"
-# port = "5432"
-# database = "django"
-# sslmode = "require"
-
-# # URL-encode the password
-# encoded_password = quote_plus(password)
-
-# # Construct the database URL with the encoded password and the new credentials
-# SQLALCHEMY_DATABASE_URL = f"postgresql://{username}:{encoded_password}@{host}:{port}/{database}?sslmode={sslmode}"
-
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-
-
 import asyncio
 from datetime import datetime
-from fastapi import FastAPI
-from pydantic import BaseModel
+import socket
 from tortoise import Tortoise, fields
 from tortoise.models import Model
 
@@ -76,35 +26,25 @@ Tortoise.init(
     modules={'models': ['__main__']},
 )
 
-app = FastAPI()
-
-class GT06Message(BaseModel):
-    imei: str
-    lat: float
-    lon: float
-    fix_time: datetime
-    speed: float
-    course: float
-
-async def write_to_postgres(pos_msg: GT06Message):
+async def write_to_postgres(pos_msg):
     await Tortoise.init(
         db_url=f"postgres://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
         modules={'models': ['__main__']},
     )
     await Tortoise.generate_schemas()
     await Position.create(
-        device_id=int(pos_msg.imei),
-        fix_time=pos_msg.fix_time,
-        position={"type": "Point", "coordinates": [pos_msg.lon, pos_msg.lat]},
-        speed=pos_msg.speed,
-        course=pos_msg.course,
+        device_id=int(pos_msg["imei"]),
+        fix_time=pos_msg["fix_time"],
+        position={"type": "Point", "coordinates": [pos_msg["lon"], pos_msg["lat"]]},
+        speed=pos_msg["speed"],
+        course=pos_msg["course"],
     )
 
-async def handle_gt06_data(data: bytes, addr):
+async def handle_gt06_data(data, addr):
     gt06_msg = {"fix_time": datetime.utcnow()}  # Modify this based on your GT06 message format
     # Process the raw GT06 message and extract relevant information
 
-    await write_to_postgres(GT06Message(**gt06_msg))
+    await write_to_postgres(gt06_msg)
 
 async def esp_tracker_server():
     esp_tracker_server_port = 64458
@@ -115,4 +55,15 @@ async def esp_tracker_server():
         await server.serve_forever()
 
 if __name__ == "__main__":
-    asyncio.run(esp_tracker_server())
+    loop = asyncio.get_event_loop()
+    coro = asyncio.start_server(handle_gt06_data, '0.0.0.0', 64458, loop=loop)
+    server = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.close()
+        loop.run_until_complete(server.wait_closed())
+        loop.close()
